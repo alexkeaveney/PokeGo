7f91d5b8ea6f511f0654b351457330a87692540a
decksnap <admin@GardaSurveillanceLaptop.local>
2017-06-04 18:53:38 +0100

    Updated code
    Map view put in
    Assets for game brought in
    Location tracking of user brought in

---
+++ PokeGo/PokeDexViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PokeDexViewController.swift
+//  PokeGo
+//
+//  Created by admin on 04/06/2017.
+//  Copyright © 2017 Alex Keaveney. All rights reserved.
+//
+
+import UIKit
+
+class PokeDexViewController: UIViewController {
+
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func mapTapped(_ sender: Any) {
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PokeGo/ViewController.swift
+++ PokeGo/ViewController.swift
@@ -7,18 +7,98 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    
+    var manager = CLLocationManager()
+    
+    var updateCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+
+        
+        //sets view controller as a CLLocationManager
+        manager.delegate = self
+        
+        //checks to see if the app is authorized to get the location
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("We are authorized")
+            
+            //Show the users location on the map
+            map.showsUserLocation = true
+            
+            //track the users location
+            manager.startUpdatingLocation()
+            
+            //timer to spawn a pokemon every 5 seconds
+            Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
+                
+                
+                
+                
+                if let coord = self.manager.location?.coordinate {
+                    let anno = MKPointAnnotation()
+                    anno.coordinate = coord
+                    anno.coordinate.latitude += (Double(arc4random_uniform(200))-100.0)/50000.0
+                    anno.coordinate.longitude += (Double(arc4random_uniform(200))-100.0)/50000.0
+                    self.map.addAnnotation(anno)
+                }
+                
+            })
+            
+        }
+        else {
+            //ask the user to use their location when they are using the app
+            manager.requestWhenInUseAuthorization()
+        }
+        
+        
+    }
+    
+    //called in a loop when the location changes
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        
+        //first three updates to get the users location centered
+        if (updateCount < 3) {
+        
+            //gets the users region
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 200, 200)
+            
+            //zoom in to users location
+            map.setRegion(region, animated: false)
+            
+            updateCount += 1
+        
+        }
+            
+        //stops updating location after three
+            
+        else {
+            manager.stopUpdatingLocation()
+        
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //if compass is clicked center the users position
+    @IBAction func centerTapped(_ sender: Any) {
+        
+        //gets the users region
+        
+        if let coord = manager.location?.coordinate {
+            
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 200, 200)
+        
+            //zoom in to users location
+            map.setRegion(region, animated: true)
+        }
     }
+    
 
 
 }
